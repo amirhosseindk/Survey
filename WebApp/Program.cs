398c88090bc264301ba55947a2e36dadc28d0744
a_daghighkia <a_daghighkia@shatel.ir>
2024-06-20 19:00:05 +0330

    fix result page problem

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -5,7 +5,6 @@ using WebApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
@@ -25,11 +24,9 @@ var app = builder.Build();
 //    await DataSeeder.SeedRolesAndAdminAsync(services);
 //}
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
